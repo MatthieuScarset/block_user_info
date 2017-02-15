65510cd40aa3ddc1168fd8b264465f2a6eaa37a8
Matt <matthieu.scarset@niji.fr>
2017-02-15 14:38:09 +0100

    Enhance block with multiple users selection

--- src/Plugin/Block/BlockUserInfo.php
+++ src/Plugin/Block/BlockUserInfo.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\block_user\Plugin\Block;
 
+use Drupal\Core\Link;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\Core\Form\FormStateInterface;
@@ -66,7 +67,7 @@ class BLockUserInfo extends BlockBase implements ContainerFactoryPluginInterface
     $plugin_definition,
     ConfigFactoryInterface $config_factory,
     EntityTypeManager $entityTypeManager,
-    AccountProxyInterface $current_user
+    AccountProxyInterface $current_account
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->configFactory = $config_factory;
@@ -74,10 +75,8 @@ class BLockUserInfo extends BlockBase implements ContainerFactoryPluginInterface
     $this->userViewBuilder = $this->entityTypeManager->getViewBuilder('user');
 
     // Get user info.
-    $this->currentAccount = $current_user;
-    $account = $this->currentAccount;
-    $uid = $account->id();
-    $this->currentUser = $this->entityTypeManager->getStorage('user')->load($uid);
+    $this->currentAccount = $current_account;
+    $this->currentUser = $this->entityTypeManager->getStorage('user')->load($this->currentAccount->id());
 
     // Get user entity display mode.
     $this->defaultViewMode = 'default';
@@ -93,7 +92,10 @@ class BLockUserInfo extends BlockBase implements ContainerFactoryPluginInterface
    * {@inheritdoc}
    */
   public function defaultConfiguration() {
-    return ['label_display' => FALSE];
+    return [
+      'label_display' => TRUE,
+      'target' => 'current',
+    ];
   }
 
   /**
@@ -101,11 +103,24 @@ class BLockUserInfo extends BlockBase implements ContainerFactoryPluginInterface
    */
   public function blockForm($form, FormStateInterface $form_state) {
     $form = parent::blockForm($form, $form_state);
-    $uid = isset($this->configuration['user']) ? $this->configuration['user'] : $this->currentUser->id();
-    $targeted_user = $this->entityTypeManager->getStorage('user')->load($uid);
+    
+    // Prepare form texts.
+    $url_add_view_mode = Link::createFromRoute(
+      $this->t('add a new view mode here'),
+      'entity.entity_view_mode.add_form',
+      ['entity_type_id' => 'user']
+    );
+    $url_account_display = Link::createFromRoute(
+      $this->t('edit displayed fields there'),
+      'entity.entity_view_display.user.default'
+    );
     $description = $this->t('Select which display mode this block should use.');
-    $help = $this->t('You can <a href="/admin/structure/display-modes/view/add/user">add a new display mode here</a> and <a href="/admin/config/people/accounts/display">edit displayed fields there</a>.');
+    $help = $this->t('You can ') . $url_add_view_mode->toString() . ' ' . $this->t('and') . ' ' . $url_account_display->toString();
+    
+    // Load referenced users entities.
+    $user_default_value = $this->getReferencedUsers();
 
+    // Build form.
     $form['userinfo'] = array(
       '#type' => 'fieldset',
       '#title' => $this->t('Settings'),
@@ -113,29 +128,42 @@ class BLockUserInfo extends BlockBase implements ContainerFactoryPluginInterface
     $form['userinfo']['view_mode'] = array(
       '#type' => 'select',
       '#options' => $this->userViewModes,
-      '#title' => $this->t('User display mode'),
+      '#title' => $this->t('Select a display mode'),
       '#description' => $description . '<br />' . $help,
       '#default_value' => isset($this->configuration['view_mode']) ? $this->configuration['view_mode'] : $this->defaultViewMode,
     );
     $form['userinfo']['target'] = array(
-      '#type' => 'checkbox',
-      '#title' => $this->t("Select a specific user."),
-      '#description' => $this->t("By default, the block displays the current user's info."),
+      '#type' => 'radios',
+      '#options' => [
+        'current' => 'Current user',
+        'author' => 'Node author',
+        'users' => 'Specific user(s)',
+      ],
+      '#title' => $this->t("Select user(s) to be retrieved"),
       '#default_value' => isset($this->configuration['target']) ? $this->configuration['target'] : FALSE,
     );
     $form['userinfo']['user'] = array(
       '#type' => 'entity_autocomplete',
       '#target_type' => 'user',
       '#selection_settings' => ['include_anonymous' => FALSE],
+      '#default_value' => $user_default_value ? $user_default_value : NULL,
+      '#tags' => TRUE,
       '#title' => $this->t('Targeted user'),
-      '#description' => $this->t('Select which user this block should display.'),
-      '#default_value' => $targeted_user ? $targeted_user : NULL,
+      '#description' => $this->t("Select which user(s) profile this block should display."),
       '#states' => array(
         'visible' => array(
-          ':input[name="settings[userinfo][target]"]' => array('checked' => TRUE),
+          array(
+            array(':input[name="settings[userinfo][target]"]' => array('value' => 'users')),
+            'or',
+            array(':input[name="settings[userinfo][target]"]' => array('value' => 'multiple')),
+          ),
         ),
         'required' => array(
-          ':input[name="settings[userinfo][target]"]' => array('checked' => TRUE),
+          array(
+            array(':input[name="settings[userinfo][target]"]' => array('value' => 'users')),
+            'or',
+            array(':input[name="settings[userinfo][target]"]' => array('value' => 'multiple')),
+          ),
         ),
       ),
     );
@@ -147,6 +175,11 @@ class BLockUserInfo extends BlockBase implements ContainerFactoryPluginInterface
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
     $userinfo = $form_state->getValue('userinfo');
+    // Empty the entity_autcomplete field.
+    if (isset($userinfo['target']) && $userinfo['target'] != 'users') {
+      $userinfo['user'] = NULL;
+    }
+    // Save configurations keys/values.
     foreach ($userinfo as $key => $value) {
       $this->configuration[$key] = $value;
     }
@@ -156,15 +189,34 @@ class BLockUserInfo extends BlockBase implements ContainerFactoryPluginInterface
    * {@inheritdoc}
    */
   public function build() {
-    if ($this->configuration['target']) {
-      $user = $this->entityTypeManager->getStorage('user')->load($this->configuration['user']);
+    $build = [];
+    $users = $this->getReferencedUsers();
+    $view_mode = isset($this->configuration['view_mode']) ? $this->configuration['view_mode'] : $this->defaultViewMode;
+    foreach ($users as $uid => $user) {
+      $build[] = $this->userViewBuilder->view($user, $view_mode);
+    }
+    return $build;
+  }
+
+  /**
+   * Load referenced users.
+   *
+   * @return bool|array
+   *   An array of loaded user entities.
+   */
+  protected function getReferencedUsers() {
+    $uids = [];
+    if (!isset($this->configuration['user'])) {
+      $uids[] = (int) $this->currentUser->id();
     }
     else {
-      $user = $this->currentUser;
+      foreach ($this->configuration['user'] as $ref) {
+        if (isset($ref['target_id'])) {
+          $uids[] = (int) $ref['target_id'];
+        }
+      }
     }
-    $view_mode = isset($this->configuration['view_mode']) ? $this->configuration['view_mode'] : $this->defaultViewMode;
-    $build = $this->userViewBuilder->view($user, $view_mode);
-    return $build;
+    return $this->entityTypeManager->getStorage('user')->loadMultiple($uids);
   }
 
 }
